dc3387d5aa897d08f733bc3e900ffb7def8c6923
keslyRojas <kesly107@gmail.com>
2025-11-13 12:03:05 -0600

    Agregación de migrations, seeders y models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,46 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
 
 class DatabaseSeeder extends Seeder
 {
+    use WithoutModelEvents;
+
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        /*User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);*/
+        $this->call([
+            //TransactionTypeSeeder::class,
+            //UsersWithWalletsSeeder::class,
+
+            //GardenPlotSeeder::class,
+
+            //InventoryItemCategorySeeder::class,
+            //InventoryItemSeeder::class,
+
+            //NotificationTypeSeeder::class,
+
+            //PlantSeeder::class,
+
+            //MissionStatusSeeder::class,
+
+            //MissionSeeder::class,
+            //MissionAssignmentStatusSeeder::class,
+            //MissionAssignmentSeeder::class,
+            PlantedCropStatusSeeder::class
+            
+        ]);
     }
 }
